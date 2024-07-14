9b3b1ab6962173a4139088454e3e38362ff6902f
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-07-14 16:38:26 +0300

    signup page done

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 function SignUp() {
+  const [PasswordVisible, setPasswordVisible] = useState(false)
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false)
+
+  const togglePassword = () => {
+    setPasswordVisible(!PasswordVisible)
+  }
+
+  const toggleConfirmPassword = () => {
+    setConfirmPasswordVisible(!confirmPasswordVisible)
+  }
+
   return (
-    <div>SignUp</div>
+    <div>
+        <div>
+          <h1>GreeNet</h1>
+          <form action="">
+            <p>Email</p>
+            <div className='inputcontainer'>
+              <input type="email" placeholder='@username or example@gmail.com' />
+              <FontAwesomeIcon icon={faEnvelope} className='icon'/>
+            </div>
+            
+            <p>Password</p>
+            <div className='inputcontainer'>
+              <input type={PasswordVisible ? 'text' : "password"} placeholder='password'/>
+              <FontAwesomeIcon icon={PasswordVisible ? faEyeSlash : faEye}  className='icon' onClick={togglePassword}/>
+            </div>
+            <p>Confirm Password</p>
+            <div className='inputcontainer'>
+              <input type={confirmPasswordVisible ? 'text' : "password"} placeholder='password'/>
+              <FontAwesomeIcon icon={confirmPasswordVisible ? faEyeSlash : faEye}  className='icon' onClick={toggleConfirmPassword}/>
+            </div>
+            <button>SignUp</button>
+            <div className='or'><span>Or</span></div>
+          </form>
+        </div>
+    </div>
   )
 }
 
